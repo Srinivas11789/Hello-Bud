a7be94b729c9e0a567b535d66e032ec53a8699ca
coolshamshuddin <coolshamshuddin@gmail.com>
2017-02-18 17:40:27 -0500

    Gradassist.py updated

--- src/gradassist.py
+++ src/gradassist.py
@@ -3,6 +3,10 @@ import requests
 import re
 import json
 
+with open('profile.json') as data_file:
+    data = json.load(data_file)
+
+
 def getcurriculum(course = ""):
     request = 'http://engineering.nyu.edu/academics/programs/computer-science-ms/curriculum/'
     try:
@@ -13,6 +17,49 @@ def getcurriculum(course = ""):
         print e
 
 
+def getCourses():
+    return data["courses"]
+
+
+def getDeadlines(type='default'):
+    deadLines = data["deadlines"]
+    if(type=='default'):
+        return deadLines;
+    else:
+        assignments = []
+        for deadLine in deadLines:
+            if(str(deadLine["type"]).lower()==str(type).lower()):
+                assignments.append(deadLine)
+        return assignments
+
+
+def getCourse(name='default', professor='default', time='default'):
+    courses = getCourses();
+
+    if name!='default':
+        for course in courses:
+            if(str(course['name']).lower()==str(name).lower()):
+                return course
+            else:
+                continue
+        return None
+    elif professor!='default':
+        for course in courses:
+            if str(course['professor']).lower()==str(professor).lower():
+                return course
+            else:
+                continue
+        return None
+
+    elif time!='default':
+        for course in courses:
+            if str(course['time']).lower()[0:3]==str(time).lower():
+                return course
+            else:
+                continue
+        return None
+
+print getCourse(time="Tue")
 # Unit Test
 
 #s = getcurriculum()

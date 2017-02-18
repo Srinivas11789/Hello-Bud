18a76e7dc5c5c0f0c4ceedb46e41598b8347c0b3
Srinivas P G <srinivas11789@gmail.com>
2017-02-18 12:09:51 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 # Project: Hello! Bud @ HackNyu
 # Main.py
 
+# Module Import
+include animation  # To Perform the Assistant Animation - In Progress
+include voice      # Assistant Voice Control - Almost Done
+include scrap      # Scrapping the Web for Nest Results - In Progress
+

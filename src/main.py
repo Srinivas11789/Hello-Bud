ef962f3232597c545dc387486941bd9dc4d41d1a
ramiscoding <ksraman.91@gmail.com>
2017-02-18 19:09:25 -0500

    Main file with support for voice

--- src/main.py
+++ src/main.py
@@ -2,7 +2,28 @@
 # Main.py
 
 # Module Import
-import animation  # To Perform the Assistant Animation - In Progress
-import voice      # Assistant Voice Control - Almost Done
-import scrapper      # Scrapping the Web for Nest Results - In Progress
-impoty gradassist.py  # Graduate Assistant
+import animation	# To Perform the Assistant Animation - In Progress
+import scrapper      	# Scrapping the Web for Nest Results - In Progress
+import gradassist  	# Graduate Assistant
+import voice		# Voice recognition features
+
+
+
+def startupFunction():
+  myStartupText = "Hello Buddy! I am Alia"
+  voice.speakVoice(myStartupText)
+
+
+def waitForInput():
+  while(1):
+    inputText = str.lower(str(voice.listenVoice()))
+    print("input : " + inputText)
+    if inputText == "google":
+      voice.speakVoice("Yes")
+      inputQuery = voice.listenVoice()
+      searchResult = scrapper.search(str(inputQuery))
+      print(searchResult)
+
+      
+startupFunction()
+waitForInput()
